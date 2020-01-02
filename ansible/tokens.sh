f13d874df2d4598c2c350ee80cfc16a78f363aae
Jack O'Sullivan <jackos1998@gmail.com>
2020-01-02 22:41:03 +0000

    Add Vultr and Hetzner cloud inventory config

--- ansible/tokens.sh
+++ ansible/tokens.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 export SCW_TOKEN=$(ansible-vault view group_vars/scaleway/vault | yq r - cloud_token)
+export VULTR_API_KEY=$(ansible-vault view group_vars/vultr/vault | yq r - cloud_token)
+export HCLOUD_TOKEN=$(ansible-vault view group_vars/hcloud/vault | yq r - cloud_token)
