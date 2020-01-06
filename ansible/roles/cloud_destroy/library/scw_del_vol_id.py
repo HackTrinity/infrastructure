4bf7a32b29d2ec72c2c75528f18cd464a2761c5a
Jack O'Sullivan <jackos1998@gmail.com>
2020-01-06 21:44:57 +0000

    Clean up Scaleway instance volumes

---
+++ ansible/roles/cloud_destroy/library/scw_del_vol_id.py
@@ -0,0 +1,35 @@
+from ansible.module_utils.scaleway import SCALEWAY_LOCATION, scaleway_argument_spec, Scaleway
+from ansible.module_utils.basic import AnsibleModule
+
+def core(module):
+    id_ = module.params['id']
+
+    account_api = Scaleway(module)
+
+    if module.check_mode:
+        module.exit_json(changed=True)
+
+    response = account_api.delete('/volumes/' + id_)
+    if response.status_code == 204:
+        module.exit_json(changed=True, data=response.json)
+    elif response.status_code == 404:
+        module.exit_json(changed=False)
+
+    module.fail_json(msg='Error deleting volume [{0}: {1}]'.format(
+        response.status_code, response.json))
+
+def main():
+    argument_spec = scaleway_argument_spec()
+    argument_spec.update(dict(
+        id=dict(required=True),
+        region=dict(required=True, choices=SCALEWAY_LOCATION.keys()),
+    ))
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    core(module)
+
+if __name__ == '__main__':
+    main()
