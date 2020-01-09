d18a69508cf528f3ecad666a068d16d1b002e34e
Jack O'Sullivan <jackos1998@gmail.com>
2020-01-09 18:56:57 +0000

    Log into Docker registry on all nodes

--- ansible/tokens.sh
+++ ansible/tokens.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export SCW_TOKEN=$(ansible-vault view group_vars/scaleway/vault | yq r - cloud_token)
-export VULTR_API_KEY=$(ansible-vault view group_vars/vultr/vault | yq r - cloud_token)
-export HCLOUD_TOKEN=$(ansible-vault view group_vars/hcloud/vault | yq r - cloud_token)
+export SCW_TOKEN=$(ansible-vault view group_vars/all/vault | yq r - scaleway_token)
+export HCLOUD_TOKEN=$(ansible-vault view group_vars/all/vault | yq r - hcloud_token)
+export VULTR_API_KEY=$(ansible-vault view group_vars/all/vault | yq r - vultr_token)
